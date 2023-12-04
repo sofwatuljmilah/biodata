b60251250ae2a9fe64cd38dfbe33825b9445193a
sopaaa <141211880+sofwatuljmilah@users.noreply.github.com>
2023-12-04 13:29:09 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let nama = "Nama: Sofwatul Jamilah";
+let kelas = "Kelas: XI";
+let jurusan = "Jurusan: PPLG";
+let umur = "Umur: 17";
+let alamat = "Alamat: Kp.gunung salikur";
+
+document.write(nama + "<br>", kelas + "<br>", jurusan + "<br>", umur + "<br>", alamat + "<br>");
